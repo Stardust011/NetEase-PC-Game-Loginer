5b251e6612f4cf0a9000b7ca6e70deab04e23367
stardust <95744877+Stardust011@users.noreply.github.com>
2025-03-13 17:51:02 +0800

    feat(gui): 初始化gui界面

---
+++ Src/GUI/interface/setting.py
@@ -0,0 +1,9 @@
+from PySide6.QtWidgets import QFrame
+
+
+class SettingsInterface(QFrame):
+    # 初始化Widget类，继承自QFrame
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)  # 调用父类的构造函数
+        # 给子界面设置全局唯一的对象名
+        self.setObjectName("SettingsInterface")
